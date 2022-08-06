663a43e575936e28ca401c53708f2c620e97a59d
Ashutosh Kumar <87602970+AshuIM@users.noreply.github.com>
2022-08-06 12:15:34 +0530

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,18 +1,3 @@
-# from flask import Flask, request, jsonify
-# app = Flask(__name__)
-#
-# # @app.route('/')
-# # def hello():
-# #     return "Hi"
-#
-#
-# if __name__ == "__main__":
-#     app.debug = True
-#     print("Starting Python Flask Server For Home Price Prediction...")
-#     app.run(port=5004)
-
-
-
 from flask import Flask, request, jsonify
 import util
 
