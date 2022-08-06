a200f74e0bd9291494e93a486251870846891405
AshuIM <ashutosh.191mn004@nitk.edu.in>
2022-08-06 11:49:11 +0530

    Folders added

---
+++ server/server.py
@@ -0,0 +1,48 @@
+# from flask import Flask, request, jsonify
+# app = Flask(__name__)
+#
+# # @app.route('/')
+# # def hello():
+# #     return "Hi"
+#
+#
+# if __name__ == "__main__":
+#     app.debug = True
+#     print("Starting Python Flask Server For Home Price Prediction...")
+#     app.run(port=5004)
+
+
+
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+@app.route('/')
+@app.route('/get_location_names', methods=['GET'])
+def get_location_names():
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+@app.route('/')
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    app.debug = True
+    util.load_saved_artifacts()
+    app.run(port=5000)
